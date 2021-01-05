cabb2deed08e2f9634b61b2d9fd6f8fdb1ce6545
Charles Hamilton <crhamiltonj@gmail.com>
2021-01-04 21:22:43 -0500

    Adjusted to correct flake8 errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,6 +14,6 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting one second...')
+                self.stdout.write('Database unavailable, waiting one second..')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
