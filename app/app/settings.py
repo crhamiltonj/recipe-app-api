bd9b68dde0ce765fa8dc0c77a9f3b0a3748d45a4
Charles Hamilton <crhamiltonj@gmail.com>
2021-01-06 00:40:45 -0500

    Added enpoint for creating and authticating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f7f+b@9&9=v_id-5vrrromq!5eyd=)i)2s(v8&btjtc4))dz79'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.21.32.242']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user'
 ]
 
 MIDDLEWARE = [
