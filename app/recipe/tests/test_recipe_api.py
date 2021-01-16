c460261bc076c56af2460d620f35b717c23aa0be
Charles Hamilton <crhamiltonj@gmail.com>
2021-01-16 15:02:03 -0500

    Added binary psycopg2

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -168,4 +168,3 @@ class PrivateRecipeApiTest(TestCase):
         self.assertEqual(ingredients.count(), 2)
         self.assertIn(ingredient1, ingredients)
         self.assertIn(ingredient2, ingredients)
-
