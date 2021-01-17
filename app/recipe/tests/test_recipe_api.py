e7496dc1ccb8f72082da703752dc1b9d2bfd2577
Charles Hamilton <crhamiltonj@gmail.com>
2021-01-17 15:03:19 -0500

    Added partial and  full updates

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -184,14 +184,14 @@ class PrivateRecipeApiTest(TestCase):
 
         recipe.refresh_from_db()
 
-        self.assertEqual(recipe.title, payload.title)
+        self.assertEqual(recipe.title, payload['title'])
         tags = recipe.tags.all()
         self.assertEqual(len(tags), 1)
         self.assertIn(new_tag, tags)
 
     def test_full_update_recipe(self):
         """Test updating a recipe with put"""
-        recipe = sample.recipe(user=self.user)
+        recipe = sample_recipe(user=self.user)
         recipe.tags.add(sample_tag(user=self.user))
         payload = {
             'title': 'Spaghetti Carbonara',
@@ -201,3 +201,10 @@ class PrivateRecipeApiTest(TestCase):
 
         url = detail_url(recipe.id)
         self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
